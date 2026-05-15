34d8a72273edfee16842a44be7426da1660b0bdb
kazuyao-eg <kazuya.oguro@englead.jp>
2026-05-15 19:57:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -544,10 +544,10 @@ def main() -> None:
     with tabs[0]:
         st.subheader("下記の形式のエクセルファイルをアップロードしてください。")
         st.markdown("")
-        st.markdown("<div style='margin-left:20px;'>・タブ名は「Versant」(大文字小文字関係なし) or 「Casec」(大文字小文字関係なし)の文字列を含むもの</div>",unsafe_allow_html=True)
-        st.markdown("<div style='margin-left:20px;'>・列は次の８列の構成：『ID』、『学習頻度』、『学習期間』、『学習日数』、『〇〇_Pre』、『〇〇_Post』、『〇〇_Post-Pre』、『Initial_Level』</div>",unsafe_allow_html=True)
-        st.markdown("<div style='margin-left:20px;'>・ID列は必ず情報が必要</div>",unsafe_allow_html=True)
-        st.markdown("<div style='margin-left:20px;'>・ID列以外の他の列は空欄でも可</div>",unsafe_allow_html=True)
+        st.markdown("<div style='margin-left:20px;'>・タブ名：「Versant」(大文字小文字関係なし) もしくは 「Casec」(大文字小文字関係なし)を含むもの。「VERSANT①」などでもOK。</div>",unsafe_allow_html=True)
+        st.markdown("<div style='margin-left:20px;'>・表は８列構成：「ID」、「学習頻度」、「学習期間」、「学習日数」、「〇〇_Pre」、「〇〇_Post」、「〇〇_Post-Pre」、「Initial_Level」</div>",unsafe_allow_html=True)
+        st.markdown("<div style='margin-left:20px;'>・「ID」列は必ず情報が必要</div>",unsafe_allow_html=True)
+        st.markdown("<div style='margin-left:20px;'>・「ID」以外の列は空欄でも可</div>",unsafe_allow_html=True)
         st.markdown("")
         st.markdown("")
 
